bb615852e582ea5b61affd487f40a0b920721d70
Wojtek <wojkor>
2025-01-30 12:09:53 +0100

    optimized draw_line

--- srcs/draw_line.c
+++ srcs/draw_line.c
@@ -6,7 +6,7 @@
 /*   By: wkornato <wkornato@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 13:44:03 by wkornato          #+#    #+#             */
-/*   Updated: 2025/01/27 13:44:49 by wkornato         ###   ########.fr       */
+/*   Updated: 2025/01/30 12:08:24 by wkornato         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@ static int	calculate_color(t_point p0, t_point p1, int iter, int line_length)
 	return (((int)((1 - (float)iter / line_length) * ((p0.color >> 16) & 0xFF)
 			+ ((float)iter / line_length)
 			* ((p1.color >> 16) & 0xFF)) << 16) | ((int)((1 - (float)iter
-				/ line_length) * ((p0.color >> 8) & 0xFF) + ((float)iter
-				/ line_length)
-			* ((p1.color >> 8) & 0xFF)) << 8) | (int)((((1 - (float)iter
-					/ line_length) * (p0.color & 0xFF)) + ((float)iter
-				/ line_length) * (p1.color & 0xFF))));
+					/ line_length) * ((p0.color >> 8) & 0xFF) + ((float)iter
+					/ line_length)
+				* ((p1.color >> 8) & 0xFF)) << 8) | (int)((((1 - (float)iter
+						/ line_length) * (p0.color & 0xFF)) + ((float)iter
+					/ line_length) * (p1.color & 0xFF))));
 }
 
 static void	line_helper(int *error, int *e2, int coords[4], t_point *p0)
@@ -37,6 +37,16 @@ static void	line_helper(int *error, int *e2, int coords[4], t_point *p0)
 		*error = *error + coords[0];
 		p0->y_pos = (int)p0->y_pos + coords[3];
 	}
+}
+
+static bool	is_out_of_bounds(t_point p0, t_map_info *map, int coords[4])
+{
+	return (((int)p0.x_pos + W_WIDTH / 2 + map->translation.x < 0
+			&& coords[1] == -1) || ((int)p0.x_pos + W_WIDTH / 2
+			+ map->translation.x >= W_WIDTH && coords[1] == 1) || ((int)p0.y_pos
+			+ W_HEIGHT / 2 + map->translation.y < 0 && coords[3] == -1)
+		|| ((int)p0.y_pos + W_HEIGHT / 2 + map->translation.y >= W_HEIGHT
+			&& coords[3] == 1));
 }
 
 void	draw_line(t_point p0, t_point p1, t_map_info *map)
@@ -60,10 +70,10 @@ void	draw_line(t_point p0, t_point p1, t_map_info *map)
 	{
 		my_mlx_pixel_put(map, (int)p0.x_pos + W_WIDTH / 2 + map->translation.x,
 			(int)p0.y_pos + W_HEIGHT / 2 + map->translation.y,
-			calculate_color(p0, p1, iter, line_length));
-		if ((int)p0.x_pos == (int)p1.x_pos && (int)p0.y_pos == (int)p1.y_pos)
+			calculate_color(p0, p1, iter++, line_length));
+		if (((int)p0.x_pos == (int)p1.x_pos && (int)p0.y_pos == (int)p1.y_pos)
+			|| is_out_of_bounds(p0, map, coords))
 			break ;
 		line_helper(&error, &e2, coords, &p0);
-		iter++;
 	}
 }
